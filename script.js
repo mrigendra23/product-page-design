a50ab12b68392fe6f0dbb5666b885334ee8d37db
mrigendra <mrigendrasingh845@gmail.com>
2024-04-20 16:19:34 +0530

    initial

---
+++ script.js
@@ -0,0 +1,25 @@
+let productImage = document.getElementById("productImg");
+let btn= document.getElementsByClassName("btn")
+
+btn[0].onclick = function(){
+    productImage.src = "image1.png"
+
+    for(bt of btn){
+        bt.classList.remove("active")
+    }
+    this.classList.add("active")
+}
+btn[1].onclick = function(){
+    productImage.src = "image2.png"
+    for(bt of btn){
+        bt.classList.remove("active")
+    }
+    this.classList.add("active")
+}
+btn[2].onclick = function(){
+    productImage.src = "image3.png"
+    for(bt of btn){
+        bt.classList.remove("active")
+    }
+    this.classList.add("active")
+}
